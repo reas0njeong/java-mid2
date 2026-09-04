5a9c815a875b96865595a880f69266a9c659e109
reas0njeong <seahorse2001@naver.com>
2026-09-05 01:11:51 +0900

    sec03: 문제와 풀이2

--- src/generic/ex3/AnimalHospitalMainV1.java
+++ src/generic/ex3/AnimalHospitalMainV1.java
@@ -1,13 +1,13 @@
-package generic.test.ex3;
+package generic.ex3;
 
 import generic.animal.Cat;
 import generic.animal.Dog;
 
-public class AnimalHospitalMainV3 {
+public class AnimalHospitalMainV1 {
 
     public static void main(String[] args) {
-        AnimalHospitalV3<Dog> dogHospital = new AnimalHospitalV3<>();
-        AnimalHospitalV3<Cat> catHospital = new AnimalHospitalV3<>();
+        AnimalHospitalV1 dogHospital = new AnimalHospitalV1();
+        AnimalHospitalV1 catHospital = new AnimalHospitalV1();
 
         Dog dog = new Dog("멍멍이1", 100);
         Cat cat = new Cat("냐옹이1", 300);
@@ -21,11 +21,11 @@ public class AnimalHospitalMainV3 {
         catHospital.checkup();
 
         // 문제1 : 개 병원에 고양이 전달
-        // dogHospital.set(cat); // 다른 타입 입력: 컴파일 오류
+        dogHospital.set(cat);
 
         // 문제2: 개 타입 변환
         dogHospital.set(dog);
-        Dog biggerDog = dogHospital.bigger(new Dog("멍멍이2", 200));
+        Dog biggerDog = (Dog) dogHospital.bigger(new Dog("멍멍이2", 200));
         System.out.println("biggerDog = " + biggerDog);
     }
 }
--- src/generic/ex3/AnimalHospitalMainV3.java
+++ src/generic/ex3/AnimalHospitalMainV3.java
@@ -1,13 +1,13 @@
-package generic.test.ex3;
+package generic.ex3;
 
 import generic.animal.Cat;
 import generic.animal.Dog;
 
-public class AnimalHospitalMainV0 {
+public class AnimalHospitalMainV3 {
 
     public static void main(String[] args) {
-        DogHospital dogHospital = new DogHospital();
-        CatHospital catHospital = new CatHospital();
+        AnimalHospitalV3<Dog> dogHospital = new AnimalHospitalV3<>();
+        AnimalHospitalV3<Cat> catHospital = new AnimalHospitalV3<>();
 
         Dog dog = new Dog("멍멍이1", 100);
         Cat cat = new Cat("냐옹이1", 300);
